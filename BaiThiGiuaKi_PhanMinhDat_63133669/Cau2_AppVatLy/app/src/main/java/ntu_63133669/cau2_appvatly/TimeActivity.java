1a751986b2cdd67538e740d0020e70d7548d5e97
phanminhdat0511 <phanminhdat69@gmail.com>
2024-03-24 15:15:47 +0700

    Thiet ke giao dien tinh thoi gian

--- BaiThiGiuaKi_PhanMinhDat_63133669/Cau2_AppVatLy/app/src/main/java/ntu_63133669/cau2_appvatly/TimeActivity.java
+++ BaiThiGiuaKi_PhanMinhDat_63133669/Cau2_AppVatLy/app/src/main/java/ntu_63133669/cau2_appvatly/TimeActivity.java
@@ -1,6 +1,9 @@
 package ntu_63133669.cau2_appvatly;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TimeActivity extends AppCompatActivity {
+    Spinner spinnervantoc, spinnerkhoangcach;
+    EditText vantoc, khoangcach, thoigian;
+    Button nutTinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_time);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
     }
 }
