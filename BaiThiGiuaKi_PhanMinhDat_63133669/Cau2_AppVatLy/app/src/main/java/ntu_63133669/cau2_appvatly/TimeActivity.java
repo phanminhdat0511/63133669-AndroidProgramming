45c3ba7bce129ec7b12a6813d0620609a3e28757
phanminhdat0511 <phanminhdat69@gmail.com>
2024-03-24 15:28:27 +0700

    tinh thoi gian

--- BaiThiGiuaKi_PhanMinhDat_63133669/Cau2_AppVatLy/app/src/main/java/ntu_63133669/cau2_appvatly/TimeActivity.java
+++ BaiThiGiuaKi_PhanMinhDat_63133669/Cau2_AppVatLy/app/src/main/java/ntu_63133669/cau2_appvatly/TimeActivity.java
@@ -21,6 +21,65 @@ public class TimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
 
+        spinnervantoc = (Spinner) findViewById(R.id.spinnerSpeed);
+        ArrayList<String> vantoc = new ArrayList<>();
+        vantoc.add("m/s");
+        vantoc.add("km/h");
+        ArrayAdapter<String> adapterVanToc = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, vantoc);
+        spinnervantoc.setAdapter(adapterVanToc);
+
+        spinnerkhoangcach = (Spinner) findViewById(R.id.spinnerDistance);
+        ArrayList<String> khoangCach = new ArrayList<>();
+        khoangCach.add("km");
+        khoangCach.add("m");
+        ArrayAdapter<String> adapterKhoangCach = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, khoangCach);
+        spinnerkhoangcach.setAdapter(adapterKhoangCach);
+
+        timView();
+        nutTinh.setOnClickListener(boLangNgheThoiGian);
 
     }
+
+    void timView(){
+        vantoc = (EditText) findViewById(R.id.edtSpeed);
+        khoangcach = (EditText) findViewById(R.id.edtDistance);
+        thoigian = (EditText) findViewById(R.id.edtKQ);
+        nutTinh = (Button) findViewById(R.id.buttonKQ);
+    }
+
+    View.OnClickListener boLangNgheThoiGian = (new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            EditText vantoc = findViewById(R.id.edtSpeed);
+            EditText khoangcach = findViewById(R.id.edtDistance);
+            EditText thoigian  = findViewById(R.id.edtKQ);
+            Spinner donviv = findViewById(R.id.spinnerSpeed);
+            Spinner donvis = findViewById(R.id.spinnerDistance);
+
+            double v1 = Double.parseDouble(vantoc.getText().toString());
+            double s = Double.parseDouble(khoangcach.getText().toString());
+            String donviVanToc = donviv.getSelectedItem().toString();
+            String donviKhoangCach = donvis.getSelectedItem().toString();
+
+            double t;
+            if( donviVanToc.equals("m/s")){
+                if(donviKhoangCach.equals("km")){
+                    t = (s / (v1 * 3.6)) * 3600;
+                }
+                else
+                    t = ((s/1000) / (v1 * 3.6)) * 3600 ;
+            }
+            else{
+                if(donviKhoangCach.equals("km")){
+                    t = (s / v1) * 3600;
+                }
+                else
+                    t = ((s/1000) / v1) * 3600;
+            }
+
+            String kq = String.valueOf(t) + " (giây)";
+            thoigian.setText(kq);
+
+        }
+    });
 }
