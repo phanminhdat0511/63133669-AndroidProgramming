8894874cfe5b846c1c1e72a07080258d06053ffd
phanminhdat0511 <phanminhdat69@gmail.com>
2024-03-24 14:57:56 +0700

    .

--- Physics/app/src/main/java/mdat/physics/TimeActivity.java
+++ Physics/app/src/main/java/mdat/physics/TimeActivity.java
@@ -5,6 +5,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,26 +20,75 @@ import java.util.ArrayList;
 
 public class TimeActivity extends AppCompatActivity {
 
+    Spinner spinnervantoc, spinnerkhoangcach;
+    EditText vantoc, khoangcach, thoigian;
+    Button nutTinh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
 
+        spinnervantoc = (Spinner) findViewById(R.id.spinnerSpeed);
+        ArrayList<String> vantoc = new ArrayList<>();
+        vantoc.add("m/s");
+        vantoc.add("km/h");
+        ArrayAdapter<String> adapterVanToc = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, vantoc);
+        spinnervantoc.setAdapter(adapterVanToc);
+
+        spinnerkhoangcach = (Spinner) findViewById(R.id.spinnerDistance);
         ArrayList<String> khoangCach = new ArrayList<>();
         khoangCach.add("km");
         khoangCach.add("m");
-
-        AutoCompleteTextView autoCompleteTextView;
-        autoCompleteTextView = findViewById(R.id.distance);
-
         ArrayAdapter<String> adapterKhoangCach = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, khoangCach);
-        autoCompleteTextView.setAdapter(adapterKhoangCach);
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-           @Override
-           public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-               String item = parent.getItemAtPosition(position).toString();
-               Toast.makeText(getApplicationContext(), "Item: " + item, Toast.LENGTH_SHORT).show();
-           }
-        });
+        spinnerkhoangcach.setAdapter(adapterKhoangCach);
+
+        timView();
+        nutTinh.setOnClickListener(boLangNgheThoiGian);
+
+
     }
+
+    void timView(){
+        vantoc = (EditText) findViewById(R.id.edtSpeed);
+        khoangcach = (EditText) findViewById(R.id.edtDistance);
+        thoigian = (EditText) findViewById(R.id.edtKQ);
+        nutTinh = (Button) findViewById(R.id.buttonKQ);
+    }
+
+    View.OnClickListener boLangNgheThoiGian = (new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            EditText vantoc = findViewById(R.id.edtSpeed);
+            EditText khoangcach = findViewById(R.id.edtDistance);
+            EditText thoigian  = findViewById(R.id.edtKQ);
+            Spinner donviv = findViewById(R.id.spinnerSpeed);
+            Spinner donvis = findViewById(R.id.spinnerDistance);
+
+            double v1 = Double.parseDouble(vantoc.getText().toString());
+            double s = Double.parseDouble(khoangcach.getText().toString());
+            String donviVanToc = donviv.getSelectedItem().toString();
+            String donviKhoangCach = donvis.getSelectedItem().toString();
+
+            double t;
+            if( donviVanToc.equals("m/s")){
+                if(donviKhoangCach.equals("km")){
+                    t = (s / (v1 * 3.6)) * 3600;
+                }
+                else
+                    t = ((s/1000) / (v1 * 3.6)) * 3600 ;
+            }
+            else{
+                if(donviKhoangCach.equals("km")){
+                    t = (s / v1) * 3600;
+                }
+                else
+                    t = ((s/1000) / v1) * 3600;
+            }
+
+            String kq = String.valueOf(t) + " (giây)";
+            thoigian.setText(kq);
+
+        }
+    });
 }
