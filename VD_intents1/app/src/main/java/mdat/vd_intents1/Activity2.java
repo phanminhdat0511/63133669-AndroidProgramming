f1a8b2dc745a14bcd1e988e701438dba7ba16cf6
phanminhdat0511 <phanminhdat69@gmail.com>
2024-04-24 12:25:05 +0700

    vdIntents1

---
+++ VD_intents1/app/src/main/java/mdat/vd_intents1/Activity2.java
@@ -0,0 +1,25 @@
+package mdat.vd_intents1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+    public void chuyenManHinh(View v){
+        Intent iChuyenManHinh = new Intent(this, MainActivity.class);
+        startActivity(iChuyenManHinh);
+    }
+}
---
+++ VD_intents1/app/src/main/java/mdat/vd_intents1/MainActivity.java
@@ -0,0 +1,25 @@
+package mdat.vd_intents1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void chuyenManHinh(View v){
+        Intent iManHinhKhac = new Intent(this, Activity2.class);
+        startActivity(iManHinhKhac);
+    }
+}
